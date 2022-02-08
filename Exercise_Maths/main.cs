79cf0b56250c356eee61618552f21b0d07b8decb
EmilLenler <lenler-e@hotmail.com>
2022-02-08 15:10:47 +0100

    Did Maths exercise succesfully"

---
+++ Exercise_Maths/main.cs
@@ -0,0 +1,9 @@
+using static System.Math;
+class Maths{
+	static void Main(){
+		double num1=Sqrt(2);
+		double num2=Pow(E,PI);
+		double num3=Pow(PI,E);
+			System.Console.Write("Sqrt of 2 is {0}, e^pi is {1}, pi^e is {2} \n",num1, num2, num3);
+	}
+}
