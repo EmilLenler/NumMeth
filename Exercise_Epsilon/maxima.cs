a64ff3f037dd14eb8284ef16e52357644f42564f
EmilLenler <lenler-e@hotmail.com>
2022-02-11 11:59:42 +0100

    Some work on the min/max ints etc.

---
+++ Exercise_Epsilon/maxima.cs
@@ -0,0 +1,13 @@
+using static System.Console;
+class hello{
+	static void Main(){
+		int i = 1;
+		while (i<i+1)
+			i++;
+		Write("Max. integer is {0}\n",i);
+		int l =1;
+		while (l>l-1)
+			l--;
+		Write("Min. integer is {0}\n",l);
+	}
+}
