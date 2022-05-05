3794968ef63c80dc3180c7551592023421c83f61
EmilLenler <lenler-e@hotmail.com>
2022-05-05 14:57:02 +0200

    Minimization finally working

--- Homeworks/Minimization/PartA/main.cs
+++ Homeworks/Minimization/PartA/main.cs
@@ -10,12 +10,35 @@ public class main{
             double res = (1-x)*(1-x)+100*(y-x*x)*(y-x*x);
             return res;
         };
-        vector xstart = new vector(1.1,1.1);
-        vector oneone = new vector(1,1);
-        vector V1 = minimization.qnewton(RosBrock,xstart);
-        V1.print($"Minimum for Rosenbrock Function (1-x)^2+100*(y-x^2)^2 is: ");
-        WriteLine($"Rosenbrock function evaluated in this point is {RosBrock(V1)}");
-
+        vector xstart = new vector(1.5,0.5);
+        xstart.print($"Starting point for guessing minimum of Rosenbrock function is:");
+        var (V1,steps1) = minimization.qnewton(RosBrock,xstart);
+        V1.print($"Minimum of Rosenbrock function is found in point:");
+        WriteLine($"Value of Rosenbrock function in this point is {RosBrock(V1)}");
+        WriteLine($"Took {steps1} steps to find minimum of Rosenbrock fct.");
+        Func<vector,double> Himmel = delegate(vector v){
+            double x =v[0];
+            double y= v[1];
+            return Pow(x*x+y-11,2)+Pow(x+y*y-7,2);
+        };
+        vector Himmel1 = new vector(3.5,1.5);
+        vector Himmel2 = new vector(-3,4);
+        vector Himmel3 = new vector(-3.5,-4);
+        vector Himmel4 = new vector(3,-1);
+        var (H1,stepsH1) =minimization.qnewton(Himmel,Himmel1);
+        var (H2,stepsH2) = minimization.qnewton(Himmel,Himmel2);
+        var (H3,stepsH3) = minimization.qnewton(Himmel,Himmel3);
+        var (H4,stepsH4) = minimization.qnewton(Himmel,Himmel4);
+        H1.print("Found first minimum H1 of Himmelblau fct in: ");
+        H2.print("Found second minimum H2 of Himmelblau fct in: ");
+        H3.print("Found third minimum H3 of Himmelblau fct in: ");
+        H4.print("Found fourth minimum H4 of Himmelblau fct in: ");
+        WriteLine($"Himmelblau function evaluated in these points respectively is: {Himmel(H1)} {Himmel(H2)} {Himmel(H3)} {Himmel(H4)}");
+        WriteLine($"Finding each minimum took respective numbers of steps: {stepsH1} {stepsH2} {stepsH3} {stepsH4}");
+        Himmel1.print($"Guess for H1 was: ");
+        Himmel2.print($"Guess for H2 was: ");
+        Himmel3.print($"Guess for H3 was: ");
+        Himmel4.print($"Guess for H4 was: ");
     }
     
 }
--- Homeworks/Minimization/PartA/minimization.cs
+++ Homeworks/Minimization/PartA/minimization.cs
@@ -3,53 +3,54 @@ using static System.Math;
 using static System.Console;
 
 public static class minimization{
-    public static vector qnewton(Func<vector,double> f, vector xstart,double eps = 1e-2, double alpha=1e-4,double eps_vec = 1e-2){
+    public static (vector,int) qnewton(Func<vector,double> f, vector xstart,double eps = 1e-6, double alpha=1e-4,double eps_vec = 1e-2){
     matrix B = matrix.id(xstart.size);
-    vector x = xstart;
-    do{
-        vector dx =-B*grad(f,x);
-        dx.print("dx is ");
-        double lambda = 1.0;
-        vector s = lambda*dx;
-        while(f(x+s)>f(x)+alpha*s%grad(f,x) && lambda >1.0/32){
-            lambda/=2;
-            WriteLine($"Lambda = {lambda}");
-            s = lambda*dx;
-        }
-        s.print($"Updating x with s = ");
-        x+=s;
-        x.print($" x = ");
-        if(lambda>1.0/32){
-            vector y = grad(f,x+s);
-            vector u = s-B*y;
-            if(Abs(u%y)>eps_vec){
-            WriteLine($"u dot y is non-zero");
-            B.update(u,u,1.0/(u%y));
-            WriteLine($"Updating B");
-            B.print($" B = ");
-            }
-        }
-        else{
-            B = matrix.id(xstart.size);
-            WriteLine($"Resetting B");
+    vector x = xstart.copy();
+    vector gradient_x =grad(f,xstart);
+    int nsteps = 0;
+    while(nsteps<999999){
+        nsteps++;
+        vector dx =-B*gradient_x;
+        //dx.print("dx is "); //DEBUG LINE
+        if(dx.norm()<Pow(2,-26)){break;}; //Stop looping if step taken is too small.
+        if(gradient_x.norm()<eps){break;} // Stop looping if gradient is already very low. 
+        double lambda = 1;
+        vector s;
+        while(true){
+            s = dx*lambda;
+            double fnew = f(x+s);
+            if(fnew<f(x)){break;} //Moving correct way!
+            if(lambda<Pow(2,-26)){ //Lambda got too small.
+                B.setid();
+                break;}
+                lambda/=2; // Divide lambda by 2 to try with smaller step.
+            }//end of while(true)
+        vector grad_new = grad(f,x+s);
+        vector y =grad_new-gradient_x;
+        vector u = s-B*y;
+        double udoty = u.dot(y);
 
+        if(Abs(udoty)>1e-6){B.update(u,u,1/udoty);};//Update B so long as we do not risk division with small number udoty.
+        x+=s;
+        gradient_x = grad_new;
         }
-    }while(grad(f,x).norm()>eps);
-    return x;
-    }
+        if(nsteps == 999999){WriteLine($"WARNING: MAXIMUM NUMBER OF STEPS USED. CONVERGENCE NOT GUARANTEED.");}
+        return(x,nsteps);
+        }//end of qnewton
     public static vector grad(Func<vector,double> f, vector x){
     vector deltax = x*Pow(2,-26);
     //Construct gradient
     vector grad = new vector(x.size);
     for(int i=0; i<x.size; i++){
         vector xnew = new vector(x.size);
+            if(Abs(deltax[i])<Pow(2,-26)) deltax[i]=Pow(2,-26);
             xnew[i] = deltax[i];
-            xnew.print($"xnew is");
+            //xnew.print($"xnew is"); //DEBUG LINE
             vector xprime = x+xnew;
             double df = f(xprime)-f(x);
             grad[i] = df/deltax[i];
         }
-        grad.print($"Gradient of f(x) at {x[0]}, {x[1]} is:");
+        //grad.print($"Gradient of f(x) at {x[0]}, {x[1]} is:"); //DEBUG LINE
         return grad;
     }
 
